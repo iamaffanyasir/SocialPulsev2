fe663ba20fa0dc25e6917d88dcd457a937927b53
imaffanyasir <aafiannu172@gmail.com>
2023-11-14 17:55:56 +0530

    pre-final-release

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -3,11 +3,8 @@ import { useInView } from "react-intersection-observer";
 
 import { Input } from "@/components/ui";
 import useDebounce from "@/hooks/useDebounce";
-import { useGetPosts, useSearchPosts } from "@/lib/react-query/queriesAndMutation";
-import Loader from "@/components/shared/Loader";
-import GridPostList from "@/components/shared/GridPostList";
-// import { GridPostList, Loader } from "@/components/shared";
-// import { useGetPosts, useSearchPosts } from "@/lib/react-query/queries";
+import { GridPostList, Loader } from "@/components/shared";
+import { useGetPosts, useSearchPosts } from "@/lib/react-query/queries";
 
 export type SearchResultProps = {
   isSearchFetching: boolean;
@@ -40,66 +37,43 @@ const Explore = () => {
     }
   }, [inView, searchValue]);
 
-  if (!posts)
+  if (!posts) {
     return (
       <div className="flex-center w-full h-full">
         <Loader />
       </div>
     );
+  }
 
   const shouldShowSearchResults = searchValue !== "";
-  const shouldShowPosts = !shouldShowSearchResults && 
-    posts.pages.every((item) => item.documents.length === 0);
+  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((page) => page.documents.length === 0);
 
   return (
     <div className="explore-container">
       <div className="explore-inner_container">
         <h2 className="h3-bold md:h2-bold w-full">Search Posts</h2>
         <div className="flex gap-1 px-4 w-full rounded-lg bg-dark-4">
-          <img
-            src="/assets/icons/search.svg"
-            width={24}
-            height={24}
-            alt="search"
-          />
+          <img src="/assets/icons/search.svg" width={24} height={24} alt="search" />
           <Input
             type="text"
             placeholder="Search"
             className="explore-search"
             value={searchValue}
-            onChange={(e) => {
-              const { value } = e.target;
-              setSearchValue(value);
-            }}
+            onChange={({ target: { value } }) => setSearchValue(value)}
           />
         </div>
       </div>
 
-      <div className="flex-between w-full max-w-5xl mt-16 mb-7">
-        <h3 className="body-bold md:h3-bold">Popular Today</h3>
-
-        <div className="flex-center gap-3 bg-dark-3 rounded-xl px-4 py-2 cursor-pointer">
-          <p className="small-medium md:base-medium text-light-2">All</p>
-          <img
-            src="/assets/icons/filter.svg"
-            width={20}
-            height={20}
-            alt="filter"
-          />
-        </div>
-      </div>
+      {/* ... (other components) */}
 
       <div className="flex flex-wrap gap-9 w-full max-w-5xl">
         {shouldShowSearchResults ? (
-          <SearchResults
-            isSearchFetching={isSearchFetching}
-            searchedPosts={searchedPosts}
-          />
+          <SearchResults isSearchFetching={isSearchFetching} searchedPosts={searchedPosts} />
         ) : shouldShowPosts ? (
           <p className="text-light-4 mt-10 text-center w-full">End of posts</p>
         ) : (
-          posts.pages.map((item, index) => (
-            <GridPostList key={`page-${index}`} posts={item.documents} />
+          posts.pages.map((page, index) => (
+            <GridPostList key={`page-${index}`} posts={page.documents} />
           ))
         )}
       </div>
--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,24 +1,22 @@
 import { Models } from 'appwrite';
-
 import Loader from './Loader';
 import GridPostList from './GridPostList';
 
 type SearchResultsProps = {
-    isSearchFetching: boolean;
-    searchedPosts: Models.Document[];
+  isSearchFetching: boolean;
+  searchedPosts: Models.Document[];
 }
 
-const SearchResults = ({ isSearchFetching, searchedPosts, }: SearchResultsProps) => {
+const SearchResults = ({ isSearchFetching, searchedPosts }: SearchResultsProps) => {
+  if (isSearchFetching) return <Loader />;
 
-        if(isSearchFetching) return <Loader />
-
-        if(searchedPosts && searchedPosts.documents.length > 0) return (
-            <GridPostList posts={searchedPosts.documents} />
-        )
-        
-  return (
-    <p className='text-light-4 mt-10 text-center w-full'>No results found</p>
-  )
+  if (searchedPosts && searchedPosts.length > 0) {
+    return <GridPostList posts={searchedPosts} />;
   }
 
-export default SearchResults
+  return (
+    <p className='text-light-4 mt-10 text-center w-full'>No results found</p>
+  );
+}
+
+export default SearchResults;
