318173c2b74438520f07cb07f1fc08c3ff77b578
imaffanyasir <aafiannu172@gmail.com>
2023-11-14 18:37:49 +0530

    fixed-padding

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -53,7 +53,7 @@ const SigninForm = () => {
 
   return (
     <Form {...form}>
-      <div className="sm:w-420 flex-center flex-col m-10">
+      <div className="sm:w-420 flex-center flex-col m-20">
         <img src="/assets/images/logo.png" alt="logo" />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
